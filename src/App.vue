d33d67f2987f9d7f920004f8ec6b359b5d539347
D34DPlayer <d34dplayer@protonmail.com>
2021-05-09 16:37:24 +0200

    Add metadata

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,53 @@ export default {
   name: "App",
   components: { Navbar },
   data: () => ({ show: true }),
+  metaInfo() {
+    const path = this.$route.path;
+    const image = require("@/assets/me.jpg");
+    const domain = process.env.VUE_APP_DOMAIN_NAME || "www.d34d.fun";
+
+    return {
+      title: "Portfolio",
+      titleTemplate: "%s | Carlos Ruiz Herrera",
+      meta: [
+        {
+          vmid: "title",
+          property: "og:title",
+          content: "Portfolio",
+        },
+        {
+          property: "og:site_name",
+          content: "Carlos Ruiz Herrera",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:image",
+          content: `https://${domain}${image}`,
+        },
+        {
+          property: "og:url",
+          content: `https://${domain}${path}`,
+        },
+        {
+          property: "og:description",
+          content:
+            "Motivated student in information technology, I have practical experience in backend and frontend development acquired during various projects and trainings.",
+        },
+        {
+          property: "og:locale",
+          content: "en_US",
+        },
+        {
+          name: "description",
+          content:
+            "Motivated student in information technology, I have practical experience in backend and frontend development acquired during various projects and trainings.",
+        },
+      ],
+    };
+  },
 };
 </script>
 
